f273063c743526601afe12e263b5db657785c911
Eddyy1410 <eddynguyen14@gmail.com>
2025-10-02 23:39:51 +0700

    Initial project

---
+++ src/main/java/com/huyntd/superapp/gundam_shop/GundamShopApplication.java
@@ -0,0 +1,13 @@
+package com.huyntd.superapp.gundam_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundamShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundamShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huyntd/superapp/gundam_shop/GundamShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huyntd.superapp.gundam_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GundamShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
